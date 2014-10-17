cfeca606b30165642bbbac39177a10d1bd7546ac
Zeeshan Khan <zkhan1093@gmail.com>
2014-10-17 20:23:05 +0530

    have processing dialog, working list on users fragment

--- src/in/siet/secure/sgi/MainActivity.java
+++ src/in/siet/secure/sgi/MainActivity.java
@@ -1,7 +1,12 @@
 package in.siet.secure.sgi;
 
+import in.siet.secure.dao.DbHelper;
+import in.siet.secure.dao.DbStructure;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -34,6 +39,7 @@ public class MainActivity extends ActionBarActivity{
 		drawerListView=(ListView)findViewById(R.id.drawer_listview);
 		drawerListView.setAdapter(new ArrayAdapter<String>(this,R.layout.list_item_drawer,panelOption));
 		drawerListView.setOnItemClickListener(new DrawerClickListner());
+		//fill_tmp_data();
 		}
 		
 	}
@@ -65,6 +71,27 @@ public class MainActivity extends ActionBarActivity{
 		Log.d(TAG,"stating login Activity");
 		startActivity(intent);
 		finish();
+	}
+
+	public void fill_tmp_data(){
+		DbHelper dbHelper=new DbHelper(getApplicationContext());
+		SQLiteDatabase db=dbHelper.getWritableDatabase();
+		ContentValues values=new ContentValues();
+		values.put(DbStructure.FcultyContactsTable._ID,1);
+		values.put(DbStructure.FcultyContactsTable.COLUMN_FNAME, "pogo");
+		values.put(DbStructure.FcultyContactsTable.COLUMN_LNAME, "gopo");
+		db.insert(DbStructure.FcultyContactsTable.TABLE_NAME,null, values);
+
+		Utility.RaiseToast(getApplicationContext(), "inserted value", 1);
+		db=dbHelper.getReadableDatabase();
+		String[] projection={
+				DbStructure.FcultyContactsTable._ID,
+				DbStructure.FcultyContactsTable.COLUMN_FNAME,
+				DbStructure.FcultyContactsTable.COLUMN_LNAME,
+		};
+		Cursor c=db.query(DbStructure.FcultyContactsTable.TABLE_NAME,projection, null,null,null,null,null);
+		c.moveToFirst();
+		Utility.RaiseToast(getApplicationContext(), c.getString(c.getColumnIndexOrThrow(DbStructure.FcultyContactsTable.COLUMN_FNAME)), 1);
 	}
 	
 	public void switch_fragment(int position){
