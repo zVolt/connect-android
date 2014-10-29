0d80338a3bf76b4c4f5086fe9e32f4dd6386b65b
Zeeshan Khan <zkhan1093@gmail.com>
2014-10-29 17:45:52 +0530

    users card view layout fixed

--- src/in/siet/secure/contants/Constants.java
+++ src/in/siet/secure/contants/Constants.java
@@ -1,7 +1,7 @@
 package in.siet.secure.contants;
 
 public interface Constants {
-	public final String SERVER="192.168.0.100";
+	public final String SERVER="192.168.191.51";
 	public final String PORT="8080";
 	public final String SOCKET=SERVER+":"+PORT;
 	public final String SPACE=" ";
